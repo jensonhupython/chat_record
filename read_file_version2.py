cc2c19d9a7d7ddb8db9bed7fabefabc15181f26f
Jenson Hu <jenson7899@gmail.com>
2020-11-20 08:33:23 +0800

    [Python] Count specific words in the content of the conversation

--- read_file_version2.py
+++ read_file_version2.py
@@ -10,24 +10,45 @@ def read_file(filename):
     return lines
 
 def convert(lines):
-    new_list = []
     person = None   #就是 NULL 的意思
+    new_list = []
     allen_word_count = 0
-    viki_work_count = 0
+    viki_word_count = 0
+    allen_sticker_count = 0
+    viki_sticker_count = 0
+    allen_image_count = 0
+    viki_image_count = 0
     for line in lines:
         split_list = line.split(' ')  #字串用split功能, 遇到空白分割
         time = split_list[0]
         name = split_list[1]
         if name == 'Allen':
             #print(split_list[2:])  # 取s[2]後面的資料
-            for msg in split_list[2:]:
-                allen_word_count += len(msg)
+            if split_list[2] == '貼圖':
+                allen_sticker_count += 1
+            elif split_list[2] == '圖片':
+                allen_image_count += 1
+            else: 
+                for msg in split_list[2:]:
+                    allen_word_count += len(msg)
+
         elif name == 'Viki':
             #print(split_list[2:])
-            for msg in split_list[2:]:
-                viki_work_count += len(msg)
-    print('Allen 說了', allen_word_count, '個字')
-    print('Viki 說了', viki_work_count, '個字')
+            if split_list[2] == '貼圖':
+                viki_sticker_count += 1  
+            elif split_list[2] == '圖片':
+                 viki_image_count += 1
+            else:          
+                for msg in split_list[2:]:
+                    viki_word_count += len(msg)
+       
+    print('Allen 說了', allen_word_count, '個字')               
+    print('Allen 說了', allen_sticker_count, '個貼圖')
+    print('Allen 說了', allen_image_count, '張圖片')
+    print('Viki 說了', viki_word_count, '個字')
+    print('Viki 說了', viki_sticker_count, '個貼圖')
+    print('Viki 說了', viki_image_count, '張圖片')
+
     return new_list
 
 def write_file(filename, new_lines):
